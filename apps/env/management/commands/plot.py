459dee4042c8d1cc0f4737acfb1cffbbba719125
NicholasPiano <nicholas.d.piano@gmail.com>
2014-11-20 16:56:53 +0000

    Plot works. Don't know why.

--- apps/env/management/commands/plot.py
+++ apps/env/management/commands/plot.py
@@ -12,6 +12,7 @@ import math
 from numpy.linalg import norm
 from scipy.stats import gaussian_kde
 from scipy.interpolate import interp1d
+import scipy.optimize as optimization
 
 class Command(BaseCommand):
     args = '<none>'
@@ -28,8 +29,9 @@ class Command(BaseCommand):
       Method: extract volume and surface area from each cell instance
 
       '''
+      x = np.linspace(0, 10**4, 10**4)
 
-      for region in Region.objects.filter(index__range=(1,2)):
+      for region in Region.objects.filter(index__range=(1,1)):
 
         #1. for each region, need upper and lower bounds in terms of gradient.
         #- get histogram of V/A
@@ -38,15 +40,24 @@ class Command(BaseCommand):
         a = []
         for cell_instance in region.cell_instances.all():
           v.append(float(cell_instance.volume))
-          v_over_a.append(float(cell_instance.volume)/float(cell_instance.surface_area))
+          v_over_a.append(float(cell_instance.volume)/float(cell_instance.surface_area+1))
           a.append(cell_instance.surface_area)
 
-        #plot histogram
-        plt.loglog(a, v, '*')
+        A = np.vstack([a, np.ones(len(a))]).T
+        m, c = np.linalg.lstsq(A, v)[0]
+        y_c = m*x
+        print([m,c])
+        plt.plot(y_c)
+
+        v = np.array(v)
+
+        #plot
+#         plt.plot(a, v, '*')
 #         plt.hist(v_over_a, 50)
+        plt.loglog(a, v, '*')
 
       #gradient lines
-      x = np.linspace(0, 10**7, 10**5)
+
       y_1p0 = x
       y_1p5 = x**1.5
       plt.plot(x, y_1p0)
