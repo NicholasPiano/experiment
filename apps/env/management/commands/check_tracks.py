5350a8da54f5bd60d6bb9541c236c23882fb53a6
NicholasPiano <nicholas.d.piano@gmail.com>
2015-03-03 21:10:04 +0000

    count recognition

--- apps/env/management/commands/check_tracks.py
+++ apps/env/management/commands/check_tracks.py
@@ -25,58 +25,71 @@ class Command(BaseCommand):
     base_input_path = '/Volumes/transport/data/cp/centre-ij/input'
     base_output_path = '/Volumes/transport/data/cp/centre-ij/tracks_cp'
 
-    for e in Experiment.objects.all():
+    for e in Experiment.objects.filter(name='050714'):
 
-      #make directory
+      # make directory
       if not os.path.exists(os.path.join(base_output_path, e.name)):
         os.mkdir(os.path.join(base_output_path, e.name))
 
       for s in e.series.all():
 
-        #make directory
+        # make directory
         if not os.path.exists(os.path.join(base_output_path, e.name, str(s.index))):
           os.mkdir(os.path.join(base_output_path, e.name, str(s.index)))
 
-        #get image set
+        # get image set
         bf = s.experiment.images.filter(series=s, channel=1)
         gfp = s.experiment.images.filter(series=s, channel=0)
 
-        #load tracks file
+        # load tracks file
         cell_instances = []
         with open(os.path.join(base_input_path, e.name, str(s.index), 'tracks.xls')) as tracks_file:
           for line in tracks_file.readlines():
             if int('0' + line.split('\t')[0])>0:
-              cell_instances.append(CellInstance(e.name, str(s.index), ' '.join(line.rstrip().split('\t'))))
+              cell_instances.append(CellInstance(' '.join(line.rstrip().split('\t'))))
 
-        #loop through timesteps
+        # load cell profiler file
+        with open(os.path.join(base_output_path, e.name, str(s.index), 'Nuclei.csv')) as cp_file:
+          for line in cp_file.readlines():
+            if line.split(',')[0]!='ImageNumber':
+              cell_instances.append(CellInstance(line, cp=True))
+
+        # loop through timesteps
+        cells = []
+
+        cell_count = 0
+        recog_count = 0
+
         for t in s.timesteps.order_by('index'):
           ci = filter(lambda c: c.frame==int(t.index)+1, cell_instances)
-          print('%s %d %d \n%s\n'%(e.name, s.index, t.index, '\n'.join([str(c) for c in ci])))
-          bf_t = bf.filter(timestep=t)
-          bf_first = bf_t.get(focus=0)
-          bf_first.load()
+          ci_track = filter(lambda c: not c.cp, ci)
+          ci_cp = filter(lambda c: c.cp, ci)
 
-          b = np.zeros(bf_first.array.shape) #blank black field
+          for cell_track in ci_track:
+            cell_count += 1
+            # if cell_track.id in [1, 2, 3, 4, 5]:
+            count = 0
+            for cell_cp in ci_cp:
+              count += cell_track.check_proximity(cell_cp)
+            recog_count += count
 
-          for c in ci:
-            # draw circle
-            xx, yy = np.mgrid[:b.shape[0], :b.shape[1]]
-            circle = (xx - c.row) ** 2 + (yy - c.column) ** 2 # distance from c
-            b[circle<10] = 255
+        print(cell_count, recog_count, float(recog_count/float(cell_count)))
 
-          # save image
-          image_path = os.path.join(base_output_path, e.name, str(s.index), 'tracks_cp_%s.png'%(('00' if int(t.index)<10 else ('0' if int(t.index)<100 else '')) + str(t.index)))
-          imsave(image_path, b)
 
 class CellInstance():
-  def __init__(self, experiment, series, line):
+  def __init__(self, line, cp=False):
     m = re.match(r'[0-9]+ (?P<id>[0-9]+) (?P<frame>[0-9]+) (?P<x>[0-9]+) (?P<y>[0-9]+) [-+]?[0-9]*\.?[0-9]+ [-+]?[0-9]*\.?[0-9]+ [0-9]+', line)
-    self.experiment = experiment
-    self.series = series
-    self.id = int(m.group('id'))
-    self.frame = int(m.group('frame'))
-    self.row = int(m.group('y'))
-    self.column = int(m.group('x'))
+    if cp:
+      m = re.match(r'(?P<frame>[0-9]+),(?P<id>[0-9]+),[0-9]+,(?P<x>[-+]?[0-9]*\.?[0-9]+),(?P<y>[-+]?[0-9]*\.?[0-9]+),.*', line)
+    self.id = int(float(m.group('id')))
+    self.cp = cp
+    self.frame = int(float(m.group('frame')))
+    self.row = int(float(m.group('y')))
+    self.column = int(float(m.group('x')))
+
+  def check_proximity(self, cell_instance):
+    # return yes if positions are within 10 pixels
+    return ((self.row-cell_instance.row)**2 + (self.column-cell_instance.column)**2 < 10.0)
 
   def __str__(self):
     return '[%s %s %d %d %d]' % (self.experiment, self.series, self.id, self.row, self.column)
