25f928c2991aaa57bfea5ecc33e5ef64d4b74ab7
NicholasPiano <nicholas.d.piano@gmail.com>
2015-03-08 02:15:30 +0000

    finished modified pmod_z
    Now works in ranges and generates both primary and secondary object
    images.

--- apps/env/management/commands/pmod_z.py
+++ apps/env/management/commands/pmod_z.py
@@ -18,6 +18,8 @@ import numpy as np
 from scipy.misc import imsave
 from scipy.ndimage.morphology import binary_dilation as dilate
 import matplotlib.pyplot as plt
+from skimage import filter as ft
+from skimage import exposure
 
 class Command(BaseCommand):
   args = '<none>'
@@ -25,7 +27,7 @@ class Command(BaseCommand):
 
   def handle(self, *args, **options):
     base_input_path = '/Volumes/transport/data/cp/centre-ij/tracks'
-    base_output_path = '/Volumes/transport/data/cp/centre-ij/check-gfp-point'
+    base_output_path = '/Volumes/transport/data/cp/centre-ij/pmod-z'
 
     # make directory
     if not os.path.exists(base_output_path):
@@ -44,6 +46,7 @@ class Command(BaseCommand):
           os.mkdir(os.path.join(base_output_path, e.name, str(s.index)))
 
         # get image
+        bf = s.experiment.images.filter(series=s, channel=1)
         gfp = s.experiment.images.filter(series=s, channel=0)
         series_image = gfp.get(focus=0, timestep__index=0)
         series_image.load()
@@ -83,21 +86,70 @@ class Command(BaseCommand):
                 # store
                 data.append({'data':mask_gfp.mean(), 'c':cell_instance.id, 'dp':dp, 'f':g.focus})
 
-          # plot data
-          for cell_instance in filter(lambda x: x.id==1, ci):
+          # z positions of cells
+          for cell_instance in ci:
             d_c = filter(lambda x: x['c']==cell_instance.id, data)
+            argmax = []
             for dp in dp_list:
               d_dp = filter(lambda x: x['dp']==dp, d_c)
               line = []
               for d in sorted(d_dp, key=lambda x: x['f']):
                 line.append(d['data'])
-              plt.plot(line, label='%d: %d'%(dp, np.argmax(line)))
+              argmax.append(np.argmax(line))
+            cell_instance.z = int(np.mean(argmax))
 
-            # save
-            plt.legend()
-            plot_path = os.path.join(base_output_path, e.name, str(s.index), 'point_c%d_t%s.tif'%(cell_instance.id, ('00' if int(t.index)<10 else ('0' if int(t.index)<100 else '')) + str(t.index)))
-            plt.savefig(plot_path)
-            plt.clf()
+          # divide z stack into chunks and group cells by chunk
+          for c in range(int((gfp_t.count()+5)/5.0)):
+            for cell_instance in ci:
+              if cell_instance.c is None and cell_instance.z >= c*5 and cell_instance.z < (c+1)*5:
+                cell_instance.c = c
+
+          for cl in np.unique([c.c for c in ci]): # for each unique level class
+            # take 5 layers of bf and 10 layers of gfp
+            # take bf max
+            # take gfp sum
+            # multiply
+
+            bf_min, bf_max = cl*5, (cl+1)*5
+            gfp_min, gfp_max = (0 if cl-2<0 else cl-2)*5, (cl+2)*5 if (cl+2)*5<gfp_t.count() else gfp_t.count()
+
+            # make images
+            bf_stack = bf.filter(timestep=t, focus__gte=bf_min, focus__lte=bf_max)
+            gfp_stack = gfp_t.filter(focus__gte=gfp_min, focus__lte=gfp_max)
+
+            bf_max_proj = np.zeros(series_image.array.shape)
+            for b in bf_stack:
+              b.load()
+              bf_max_proj[bf_max_proj < b.array] = b.array[bf_max_proj < b.array]
+
+            gfp_sum_proj = np.zeros(series_image.array.shape)
+            for g in gfp_stack:
+              g.load()
+              gfp_sum_proj += g.array
+
+            gfp_threshold = exposure.equalize_hist(gfp_sum_proj)
+            gfp_threshold[gfp_sum_proj<gfp_sum_proj.mean()] = 0
+
+            gfp_smooth = ft.gaussian_filter(gfp_threshold, sigma=5)
+
+            # save output
+            output_image = gfp_smooth * bf_max_proj
+            sec_image_path = os.path.join(base_output_path, e.name, str(s.index), 'secondary_t%s_cl%d.tif'%((len(str(s.timesteps.count()))-len(str(t.index)))*'0' + str(t.index), cl))
+
+            imsave(sec_image_path, output_image)
+
+            # primary objects
+            black_field = np.zeros(series_image.array.shape)
+
+            for cell_instance in filter(lambda x: x.c==cl, ci):
+              # draw circle
+              xx, yy = np.mgrid[:black_field.shape[0], :black_field.shape[1]]
+              circle = (xx - cell_instance.row) ** 2 + (yy - cell_instance.column) ** 2 # distance from c
+              black_field[circle<15] = 255 # radius of 10 px
+
+            pri_image_path = os.path.join(base_output_path, e.name, str(s.index), 'primary_t%s_cl%d.tif'%((len(str(s.timesteps.count()))-len(str(t.index)))*'0' + str(t.index), cl))
+
+            imsave(pri_image_path, black_field)
 
 class CellInstance():
   def __init__(self, experiment, series, line):
@@ -108,7 +160,8 @@ class CellInstance():
     self.frame = int(m.group('frame'))
     self.row = int(m.group('y'))
     self.column = int(m.group('x'))
-    self.z = None #set from gfp -> argmax from focus
+    self.z = None # set from gfp -> argmax from focus
+    self.c = None # level class
 
   def __str__(self):
     return '[%s %s %d %d %d]' % (self.experiment, self.series, self.id, self.row, self.column)
